c94c3f35a6d8390bac71acdf8782dd1d13090898
Jake Rundall <rundall@illinois.edu>
2022-04-20 16:21:07 -0500

    SVCPLAN-736 - create profile_mysql_server
    initial commit (ported initially from LSST to ngale-control/profile::mariadb_server)
    
    add $other_depencencies
    
    remove $mysql_parentdir (make Puppet figure out what the parents are)
    
    add $create_mysql_home boolean to make managing that folder optional
    
    add $manage_yumrepo boolean to make managing the repo optional
    
    make mysql_{gid,uid} and user/group/dir mgmt optional

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# puppet-profile_mysql_server
+# profile_mysql_server
+
+![pdk-validate](https://github.com/ncsa/puppet-profile_mysql_server/workflows/pdk-validate/badge.svg)
+![yamllint](https://github.com/ncsa/puppet-profile_mysql_server/workflows/yamllint/badge.svg)
+
+NCSA Common Puppet Profiles - install and configure MySQL/MariaDB Server
+
+Oriented towards MariaDB.
+
+Intends to offer support for stateful or stateless nodes, but generally with persistent storage for the DB.
+
+
+## Table of Contents
+
+1. [Description](#description)
+1. [Setup](#setup)
+1. [Usage](#usage)
+1. [Dependencies](#dependencies)
+1. [Reference](#reference)
+
+
+## Description
+
+This module manages additional resource dependencies for MySQL/MariaDB Server. It includes the
+[puppetlabs-mysql](https://github.com/puppetlabs/puppetlabs-mysql) module, which actually installs
+and performs most configuration of MySQL/MariaDB.
+
+
+## Setup
+
+Include profile_mysql_server in a puppet profile file:
+```
+include ::profile_mysql_server
+```
+
+
+## Usage
+
+The defaults for this profile are relatively "sane" but it may be necessary to override them and/or override settings for the underlying puppetlabs/mysql module.
+
+One of the main things that the profile can do is to create a mysql user and group with specified
+UID and GIDs. It can also create associated folders:
+```
+$mysql_home (e.g., /var/lib/mysql)
+$mysql_logdir (e.g., /var/log/mysql)
+$mysql_run (e.g., /var/run/mysql)
+```
+
+The user and group and associated folders will ONLY be created if both $mysql_uid and $mysql_gid are
+specified.
+
+This functionality may be desirable a case like the following:
+- The RPMs used to install MySQL/MariaDB do not create user/group with IDs that are guaranteed to be consistent, AND
+- The node is stateless but has persistent storage for the DB.
+
+If the selection of UID and GID is important you can provide IDs as follows:
+```yaml
+profile_mysql_server::mysql_gid: 238
+profile_mysql_server::mysql_uid: 238
+```
+
+If they are not provided, it is assumed that the user and group have already been created or will be
+created when the software is installed, and that the IDs are usable.
+
+For use with a Slurm scheduler running on a stateless node that has stateful storage mounted by Puppet
+at /var/lib/mysql, the following Hiera data generally makes sense (along with the possible specification
+of $mysql_gid and $mysql_uid):
+```yaml
+profile_mysql_server::create_mysql_home: false
+profile_mysql_server::dbs: {}
+profile_mysql_server::other_dependencies:
+  - "Mount['/var/lib/mysql']"
+```
+
+
+## Dependencies
+
+[puppetlabs/mysql](https://github.com/puppetlabs/puppetlabs-mysql)
+
+
+## Reference
+
+See: [REFERENCE.md](REFERENCE.md)
---
+++ manifests/init.pp
@@ -0,0 +1,173 @@
+# @summary Manage a MySQL/MariaDB server configuration.
+#
+# @param create_mysql_home
+#   Should the mysql_home be created?
+#
+#   NOTE: $mysql_gid and $mysql_uid must also be specified for this to do anything.
+#
+# @param dbs
+#   Raw data to define mysql::db resources.
+#
+# @param manage_yumrepo
+#   Should the mysql Yumrepo be managed? Must also provide
+#   Hiera data for the $yumrepo param.
+#
+# @param mysql_gid
+#   Optionally define the GID for the mysql group (and PGID for the mysql user).
+#   Must also provide $mysql_uid. If both $mysql_gid and $mysql_uid are provided,
+#   the mysql user and group will be created as will various directories.
+#
+# @param mysql_groupname
+#   The name of the mysql group.
+#
+#   NOTE: $mysql_gid and $mysql_uid must also be specified for this to do anything.
+#
+# @param mysql_home
+#   The home dir of the mysql user.
+#
+#   NOTE: $mysql_gid and $mysql_uid must also be specified for this to do anything.
+#
+# @param mysql_logdir
+#   Location of MySQL log files.
+#
+#   NOTE: $mysql_gid and $mysql_uid must also be specified for this to do anything.
+#
+# @param mysql_rundir
+#   Location of run directory (generally a sub-directory of /var/run/).
+#
+#   NOTE: $mysql_gid and $mysql_uid must also be specified for this to do anything.
+#
+# @param mysql_uid
+#   Optionally define the UID of the mysql user. Must also provide $mysql_gid. If
+#   both $mysql_gid and $mysql_uid are provided, the mysql user and group will be
+#   created as will various directories.
+#
+# @param mysql_username
+#   Username of the mysql user.
+#
+#   NOTE: $mysql_gid and $mysql_uid must also be specified for this to do anything.
+#
+# @param other_dependencies
+#   Other depencies to handle prior to other MySQL setup, specified as resources, e.g.:
+#     - "Mount['/var/lib/mysql']"
+#
+# @param yumrepo
+#   Raw params for a yumrepo resource from which to install MySQL/MariaDB.
+#
+# @example
+#   include profile_mysql_server
+#
+class profile_mysql_server (
+
+  Boolean           $create_mysql_home,
+  Hash              $dbs,
+  Boolean           $manage_yumrepo,
+  String            $mysql_groupname,
+  String            $mysql_home,
+  String            $mysql_logdir,
+  String            $mysql_rundir,
+  String            $mysql_username,
+  Array             $other_dependencies,
+  Hash              $yumrepo,
+  Optional[Integer] $mysql_gid,
+  Optional[Integer] $mysql_uid,
+
+) {
+
+  include ::mysql::server
+
+  if ($mysql_gid) and ($mysql_uid) {
+    # if both $mysql_gid and $mysql_uid, create various directories
+    # and manage user and group
+
+    $dir_defaults = {
+      before => Class['::mysql::server::install'],
+      ensure => directory,
+      group  => $mysql_gid,
+      owner  => $mysql_uid,
+    }
+
+    if $mysql_logdir {
+      file { $mysql_logdir:
+        * => $dir_defaults,
+      }
+    }
+    if $mysql_rundir {
+      file { $mysql_rundir:
+        * => $dir_defaults,
+      }
+    }
+
+    if $create_mysql_home {
+      # Ensure parents of target dir exist, if needed (excluding / )
+      $dirparts = reject( split( $mysql_home, '/' ), '^$' )
+      $numparts = size( $dirparts )
+      if ( $numparts > 1 ) {
+        each( Integer[2,$numparts] ) |$i| {
+          ensure_resource(
+            'file',
+            reduce( Integer[2,$i], $mysql_home ) |$memo, $val| { dirname( $memo ) },
+            { 'ensure' => 'directory' }
+          )
+        }
+      }
+
+      file { $mysql_home:
+        * => $dir_defaults,
+      }
+    }
+
+    group { $mysql_groupname:
+      ensure => 'present',
+      before => Class['::mysql::server::install'],
+      gid    => $mysql_gid,
+    }
+
+    user { $mysql_username:
+      ensure         => 'present',
+      before         => Class['::mysql::server::install'],
+      uid            => $mysql_uid,
+      gid            => $mysql_gid,
+      forcelocal     => true,
+      home           => $mysql_home,
+      managehome     => $create_mysql_home,
+      password       => '!!',
+      purge_ssh_keys => true,
+      shell          => '/sbin/nologin',
+      comment        => 'MySQL server',
+    }
+
+  } elsif $mysql_gid {
+
+    # we've specified $mysql_gid but NOT $mysql_uid
+    fail('you must provide both (or neither) mysql_gid and mysql_uid')
+
+  } elsif $mysql_uid {
+
+    # we've specified $mysql_uid but NOT $mysql_gid
+    fail('you must provide both (or neither) mysql_gid and mysql_uid')
+
+  }
+
+  $other_dependencies.each | $dep | {
+    $dep -> Class['::mysql::server::install']
+  }
+
+  if ! empty($yumrepo) {
+    if $manage_yumrepo {
+      $yumrepo_defaults = {
+        ensure  => present,
+        enabled => true,
+      }
+      ensure_resources( 'yumrepo', $yumrepo, $yumrepo_defaults )
+    }
+    Yumrepo[$yumrepo] -> Class['::mysql::server::install']
+  }
+
+  each($dbs) | $db_name, $db_data | {
+    mysql::db { $db_name:
+      * => $db_data,
+    }
+  }
+
+}
